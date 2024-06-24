33190f2d5b8ea9646ec37801b234300aed986620
tomergamedev <60105117+tomergamedev@users.noreply.github.com>
2024-06-24 14:53:09 +0300

    Basic player movement and animations.

---
+++ Assets/Scripts/CursorHider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CursorHider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
